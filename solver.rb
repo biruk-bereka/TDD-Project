9cf25f2dd0b846216c65b78a542eefaecc87329e
biruk-bereka <birukbeletech1212@gmail.com>
2023-04-21 16:49:22 +0300

    Fix linters error

--- solver.rb
+++ solver.rb
@@ -1,7 +1,8 @@
 class Solver
-    def factorial(n) 
-        raise ArgumentError, "Argument must be a non-negative integer" unless n.is_a?(Integer) && n >= 0
-        return 1 if n == 0
-        (1..n).reduce(:*)
-    end
+  def factorial(_num)
+    raise ArgumentError, 'Argument must be a non-negative integer' unless n.is_a?(Integer) && n >= 0
+    return 1 if n.zero?
+
+    (1..n).reduce(:*)
+  end
 end
