a739a9dfd47edd163645db69366252c360224dd3
biruk-bereka <birukbeletech1212@gmail.com>
2023-04-21 16:46:24 +0300

    Write a factorial method to pass the test

---
+++ solver.rb
@@ -0,0 +1,7 @@
+class Solver
+    def factorial(n) 
+        raise ArgumentError, "Argument must be a non-negative integer" unless n.is_a?(Integer) && n >= 0
+        return 1 if n == 0
+        (1..n).reduce(:*)
+    end
+end
